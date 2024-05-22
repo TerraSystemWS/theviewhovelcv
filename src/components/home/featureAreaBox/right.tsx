2a1636e6622f3348587f5d08bd298bc64e8ac6a8
Ailton Duarte <adidas.coyas@gmail.com>
2024-05-22 21:57:03 -0100

    feat: new featuresArea component

---
+++ src/components/home/featureAreaBox/right.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+interface FeatureProps {
+  subtitle: string
+  title: string
+  description: string
+  imageUrl: string
+  altText: string
+}
+
+const Right: React.FC<FeatureProps> = ({
+  subtitle,
+  title,
+  description,
+  imageUrl,
+  altText,
+}) => {
+  return (
+    <div className="row align-items-center bg-right mb-60">
+      <div className="col-xl-6 col-lg-6 order-last order-lg-first">
+        <div className="feature__area-left">
+          <div className="feature__area-left-title">
+            <span className="subtitle__one">{subtitle}</span>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <a className="theme-border-btn" href="services-details.html">
+              Read More<i className="fal fa-long-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-6 col-lg-6">
+        <div className="feature__area-image">
+          <img className="img__full" src={imageUrl} alt={altText} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Right
