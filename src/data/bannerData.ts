01efba041ab9c8876d4ca1cbe51866ee7a51328c
Ailton Duarte <adidas.coyas@gmail.com>
2024-05-22 22:42:11 -0100

    feat: create banner data

---
+++ src/data/bannerData.ts
@@ -0,0 +1,15 @@
+interface BannerData {
+  title: string
+  subtitle: string
+  imageLink: string
+  videoLink: string
+}
+
+const bannerData: BannerData = {
+  title: 'The Best Hotel',
+  subtitle: 'Deals in Fogo',
+  imageLink: 'assets/fotos/banner-1.jpg',
+  videoLink: 'https://www.youtube.com/watch?v=JhVtUBct7ik',
+}
+
+export default bannerData
