05d11bc317785a49f25fabdae2305645825819ac
Ailton Duarte <adidas.coyas@gmail.com>
2024-05-22 23:30:39 -0100

    feat: temporary post data

---
+++ src/data/postsData.ts
@@ -0,0 +1,30 @@
+export interface Post {
+  date: string
+  title: string
+  image: string
+  link: string
+}
+
+const postsData: Post[] = [
+  {
+    date: 'July 27, 2022',
+    title: 'The ultimate guide to finding the best hotels in your area.',
+    image: 'assets/img/blog/blog-1.jpg',
+    link: 'blog-details.html',
+  },
+  {
+    date: 'July 27, 2022',
+    title: 'Book a room Today most Affordable Rates.',
+    image: 'assets/img/blog/blog-2.jpg',
+    link: 'blog-details.html',
+  },
+  {
+    date: 'July 27, 2022',
+    title: 'Hotel Booking is the best choice for hotel booking.',
+    image: 'assets/img/blog/blog-3.jpg',
+    link: 'blog-details.html',
+  },
+  // Add more posts as needed
+]
+
+export default postsData
