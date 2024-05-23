c2fb46401d9a1b69e225352f391c8af04761b697
Ailton Duarte <adidas.coyas@gmail.com>
2024-05-23 01:10:40 -0100

    fix: change local

--- src/data/bannerData.ts
+++ src/data/bannerData.ts
@@ -8,7 +8,7 @@ interface BannerData {
 const bannerData: BannerData = {
   title: 'The Best Hotel',
   subtitle: 'Deals in Fogo',
-  imageLink: '/assets/fotos/banner-1.jpg',
+  imageLink: '/assets/img/banner-1.jpg',
   videoLink: 'https://www.youtube.com/watch?v=JhVtUBct7ik',
 }
 
