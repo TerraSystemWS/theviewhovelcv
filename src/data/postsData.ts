d6a65e64c5e738c6c06774da5baf97362adc8d12
Ailton Duarte <adidas.coyas@gmail.com>
2024-06-20 16:02:45 -0100

    feat: new data type

--- src/data/postsData.ts
+++ src/data/postsData.ts
@@ -2,36 +2,52 @@ export interface Post {
   date: string
   title: string
   image: string
-  link: string
+  slug: string
+  author: string
+  description: string
 }
 
-const postsData: Post[] = [
-  // Add more posts as needed
-]
-{
-  /**
+// const postsData: Post[] = [
+//   // Add more posts as needed
+// ]
+// {
+//   /**
 const postsData: Post[] = [
   {
     date: 'July 27, 2022',
     title: 'The ultimate guide to finding the best hotels in your area.',
-    image: 'assets/img/blog/blog-1.jpg',
-    link: 'blog-details.html',
+    image: '/assets/img/blog/blog-1.jpg',
+    slug: 'the-ultimate-guide-to-finding-the-best-hotels-in-your-area',
+    author: 'admin',
+    description: 'description',
   },
   {
     date: 'July 27, 2022',
     title: 'Book a room Today most Affordable Rates.',
-    image: 'assets/img/blog/blog-2.jpg',
-    link: 'blog-details.html',
+    image: '/assets/img/blog/blog-2.jpg',
+    slug: 'book-a-room-today-most-affordable-Rates',
+    author: 'admin',
+    description: 'description',
   },
   {
     date: 'July 27, 2022',
     title: 'Hotel Booking is the best choice for hotel booking.',
-    image: 'assets/img/blog/blog-3.jpg',
-    link: 'blog-details.html',
+    image: '/assets/img/blog/blog-3.jpg',
+    slug: 'hotel-booking-is-the-best-choice-for-hotel-booking',
+    author: 'admin',
+    description: 'description',
+  },
+  {
+    date: 'July 28, 2022',
+    title: 'Booking is the best choice for hotel booking.',
+    image: '/assets/img/blog/blog-3.jpg',
+    slug: 'booking-is-the-best-choice-for-hotel-booking',
+    author: 'admin',
+    description: 'description',
   },
   // Add more posts as needed
 ]
- */
-}
+//  */
+// }
 
 export default postsData
