7d6beb0409b06a6e8f7a22c09b88d85d2fc26402
Ailton Duarte <adidas.coyas@gmail.com>
2024-09-02 15:02:16 -0100

    fix: more testes

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -29,8 +29,8 @@ export const sendEmail = async (dto: Inputs) => {
   try {
     // Send the email
     const info = await transport.sendMail({
-      from: process.env.MAIL_USER, // Include sender's name and email address
-      to: email, // Recipient address
+      from: email, // Include sender's name and email address
+      to: process.env.MAIL_USER, // Recipient address
       subject: `${name} - ${subject}`, // Add subject
       html: `
       <p>${name}</p>
